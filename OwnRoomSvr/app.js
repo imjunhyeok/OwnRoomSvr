e4b81e3bcf6ccc3b165c9c40b67d74de30d0febe
jun <amazons2@hanmil.net>
2017-06-01 01:14:34 +0900

    170601

--- OwnRoomSvr/app.js
+++ OwnRoomSvr/app.js
@@ -35,28 +35,55 @@ var assert = require('assert');
 var url = 'mongodb://localhost:27017/ownroom';
 var database;
 app.get('/user/info', function(req, res){
-	var collection = database.collection('userinfo');
-	collection.find({}).toArray(function(err, docs){
-		assert.equal(err, null);
-		console.log("Found the following records");
-		res.json(docs);
-	});
-});
-
-app.get('/user/signin', function(req, res){
-	var collection = database.collection('userinfo');
+	console.log(req.param('usrid'));
 	var usrid = req.param('usrid');
-	collection.find({"usrid": usrid}).toArray(function(err, docs){
-		assert.equal(err, null);
-		console.log("Found the following records");
-		res.json(docs);
-	});
+	var collection = database.collection('userinfo');
+	// 파라미터가 없는 경우 데이터 전체를 넘겨줌
+	if( usrid === undefined){
+		collection.find({}).toArray(function(err, docs){
+			assert.equal(err, null);
+			console.log("Found the following records");
+			res.json(docs);
+		});
+	}
+	// 파라미터가 있는 경우
+	else {
+		collection.findOne({"usrid":usrid}, function(err,docs){
+			if(err) throw err;
+			res.send(docs);
+		});
+	}
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+app.post('/user/updateUserInfo', function(req, res){
+	var usrid = req.param('usrid');
+	var usrpwd = req.param('usrpwd');
+	var usrname = req.param('usrname');
+	var usrregdate = req.param('usrregdate');
+	var collection = database.collection('userinfo');
+	collection.insert({
+		'usrid' : usrid,
+		'usrpwd' : usrpwd,
+		'usrname' : usrname,
+		'usrregdate' : usrregdate
+	});
+	console.log('Success to insert userinfo data');
+});
+
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
   MongoClient.connect(url, function(err, db){
 	  console.log("Connected sucessfully to server");
 	  database = db;
   });
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket){
+	socket.emit('toclient', { msg : 'Welcome!'});
+	socket.on('fromclient', function(data){
+		socket.broadcast.emit('toclient', data);
+		console.log('Message from client : ' + data.msg);
+	});
 });
